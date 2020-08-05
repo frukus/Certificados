f2d3543e5e98c6c82fe7769b01df132f2ca329df
David Adriazola Muriel <daddriel@MacBook-Air-de-David.local>
2020-08-05 18:56:55 -0400

    Estilos

--- src/components/cer.vue
+++ src/components/cer.vue
@@ -1,10 +1,43 @@
 <template>
-  <div>
-    <h1>Certisss</h1>
-    <div>
-      {{ hhh.name }} {{hhh.telf}}
+  <div id="TDCE0001"  class = "full_screen" >
+    <div class = "webinar_title">
+        <img class="qr_container" src="../assets/logo.svg">
+        <h1>Certificado</h1>
+        <div class="img_container">
+          <img class="img_size" src="../assets/chamillo.svg">
+          <img class="img_size" src="../assets/classroom.svg">
+          <img class="img_size" src="../assets/moodle.svg">
+        </div>
+        <p>Esta página valida la emisión del certificado <strong>{{hhh.id}}</strong> emitido por Overcron, a continuación se muestra los datos de la emision correspondiente.</p>
+        <table>
+          <tbody>
+              <tr>
+                  <td class="td_iz">
+                      Receptor:
+                  </td>
+                  <td>
+                      {{ hhh.name }}
+                  </td>
+              </tr>
+              <tr>
+                  <td class="td_iz">
+                      ID:
+                  </td>
+                  <td>
+                      {{hhh.id}}
+                  </td>
+              </tr>
+              <tr>
+                  <td class="td_iz">
+                      WEBINAR:
+                  </td>
+                  <td>
+                      Transformación digital en centros educativos.
+                  </td>
+              </tr>
+          </tbody>
+      </table>
     </div>
-    <p><router-link to="/about">Back</router-link></p>
   </div>
 </template>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <ul v-for="item of certis" :key="item.id">
-      <li>
-        <h1> <router-link :to="'/prueba/' + item.id" exact> {{item.id}} </router-link> {{item.name}} </h1>
-      </li>
-    </ul>
+  <div class="full_screen2">
+    <div class="contenedor">
+      <h1 style="color:white;">ID de Certificados</h1>
+      <ul style="padding:0;">
+        <li class="lista" v-for="item of certis" :key="item.id">
+          <spam> <router-link class="text" :to="'/prueba/' + item.id" exact> {{item.id}} </router-link></spam>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -20,5 +22,33 @@ export default {
 </script>
 
 <style>
+.full_screen2{
+  background-image: linear-gradient(to right, rgba(248, 27, 63, 0.8), rgb(98, 54, 153, 1)),
+  url("../../src/assets/fondo.jpeg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100vh;
+  min-width: 100%;
+  display: flex;
+  flex-direction:column ;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+}
+.lista{
+  list-style-type: none;
+  text-align: center;
+  font-size: 1em;
+}
+.contenedor{
+  margin-top: 50px;
+}
+
+@media (max-width: 700px){
+  .contenedor{
+    margin-top: 100px;
+  }
+}
 
 </style>
