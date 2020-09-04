7adb81f06dbe045703f9e51056bf052e3a0c5d34
David Adriazola Muriel <daddriel@MacBook-Air-de-David.local>
2020-09-03 23:51:25 -0400

    add

--- src/store/index.js
+++ src/store/index.js
@@ -50,7 +50,9 @@ export default new Vuex.Store({
       { name: 'KEVIN DANIEL ROJAS', id: 34704 },
       { name: 'JIMENA ALEJANDRA PUYAL PEREZ', id: 34705 },
       { name: 'CARMEN MERCEDES CALDERON QUISPE', id: 34706 },
-      { name: 'ARMANDO VACA CITA', id: 34707 }
+      { name: 'ARMANDO VACA CITA', id: 34707 },
+      { name: 'TEREZA JAQUELINE SORIA GALVARRO', id: 34708 },
+      { name: 'ANDREA TASTACA GASPAR', id: 34709 }
     ]
   },
   mutations: {
